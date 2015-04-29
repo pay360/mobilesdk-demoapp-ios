4520f49a8b2de13218bb2a6870b96235da402526
Owen Cliffe <owencliffe@paypoint.net>
2015-04-29 10:09:02 +0100

    removing framework

--- getdeps.sh
+++ getdeps.sh
@@ -7,9 +7,11 @@ if [ -z "$FRAMEWORK_VERSION" ] ; then
    FRAMEWORK_VERSION=1.0.0-SNAPSHOT 
 fi 
 
+rm -rf Paypoint.framework Paypoint.framework.zip
+
 echo Downloading PayPoint framework version $FRAMERWORK_VERSION from Nexus 
 
-ARTIFACT_URL="${NEXUS_ROOT}/service/local/artifact/maven/redirect?r=blue-snapshots&g=net.paypoint&a=mobilesdk-ios&v=${FRAMEWORK_VERSION}&e=framework"
+ARTIFACT_URL="${NEXUS_ROOT}/service/local/artifact/maven/redirect?r=blue-snapshots&g=net.paypoint&a=mobilesdk-ios&v=${FRAMEWORK_VERSION}&e=zip"
 
-
-curl  -Lf ${ARTIFACT_URL} -o  Paypoint.framework 
+curl  -Lf ${ARTIFACT_URL} -o  Paypoint.framework.zip
+ditto -xz Paypoint.framework.zip Paypoint.framework  
