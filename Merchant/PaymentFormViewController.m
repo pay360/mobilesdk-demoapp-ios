7c9c203aeed15b9a61e67c3a44bf9d4fa65e3ae3
Rob Nash <robnash@paypoint.com>
2015-06-02 17:33:20 +0100

    Merchant payment manager removed and it's logic repositioned so that it is in one place and avoids any confusion for the reader.  New error case handled where indeterminate state results in presentation of dialogue for user to optionally check the status of a payment.

--- Merchant/PaymentFormViewController.m
+++ Merchant/PaymentFormViewController.m
@@ -11,13 +11,40 @@
 #import "OutcomeViewController.h"
 #import "NetworkErrorManager.h"
 #import "ColourManager.h"
-#import "MerchantPaymentManager.h"
+#import "MerchantServer.h"
+#import "EnvironmentManager.h"
 
-@interface PaymentFormViewController () <PaymentManagerDelegate>
-@property (nonatomic, strong) MerchantPaymentManager *paymentManager;
+@interface PaymentFormViewController ()
+@property (nonatomic, strong) PPOPaymentManager *paymentManager;
+@property (nonatomic, strong) PPOPayment *currentPayment;
+@property (nonatomic, strong) PPOCredentials *credentials;
 @end
 
 @implementation PaymentFormViewController
+
+-(PPOPaymentManager *)paymentManager {
+    
+    if (_paymentManager == nil) {
+        
+        /*
+         *A selection of environments are available.
+         *Envionrments differ by baseURL.
+         *A list of baseURL's are accessible via 'PPOEnvironment' keys.
+         */
+        PPOEnvironment currentEnvironment = [EnvironmentManager currentEnvironment];
+        
+        /*
+         *Alternatively, a custom URL can be passed in here.
+         */
+        NSURL *baseURL;
+        
+        baseURL = [PPOPaymentBaseURLManager baseURLForEnvironment:currentEnvironment];
+        
+        _paymentManager = [[PPOPaymentManager alloc] initWithBaseURL:baseURL];
+        
+    }
+    return _paymentManager;
+}
 
 -(void)viewDidLoad {
     
@@ -58,56 +85,99 @@
         
     } else if (self.animationManager.animationState == LOADING_ANIMATION_STATE_ENDED) {
         
-        PPOPayment *payment = [MerchantPaymentManager buildPaymentExampleWithDetails:self.form];
-        [self.paymentManager attemptPayment:payment];
+        PPOBillingAddress *address = [PPOBillingAddress new];
+        address.line1 = @"Street 1";
+        address.line2 = @"Street 2";
+        address.line3 = @"Street 3";
+        address.line4 = @"Street 4";
+        address.city = @"City";
+        address.region = @"Region";
+        address.postcode = @"Postcode";
+        address.countryCode = @"Country Code";
+        
+        PPOTransaction *transaction = [PPOTransaction new];
+        transaction.currency = @"GBP";
+        transaction.amount = @100;
+        transaction.transactionDescription = @"A desc";
+        transaction.merchantRef = [NSString stringWithFormat:@"mer_%.0f", [[NSDate date] timeIntervalSince1970]];
+        transaction.isDeferred = @NO;
+        
+        PPOCreditCard *card = [PPOCreditCard new];
+        card.pan = self.form.cardNumber;
+        card.cvv = self.form.cvv;
+        card.expiry = self.form.expiry;
+        card.cardHolderName = @"Dai Jones";
+        
+        PPOPayment *payment = [PPOPayment new];
+        payment.transaction = transaction;
+        payment.card = card;
+        payment.address = address;
+        
+        self.currentPayment = payment;
+        
+        /*
+         *Payments require fresh credentials, each time a payment request is made.
+         *Optional validation can be performed here, before we begin this process.
+         */
+        NSError *invalid = [PPOPaymentValidator validatePayment:payment];
+        
+        if (invalid) {
+            [self handleError:invalid];
+            return;
+        }
+        
+        [self.animationManager hideFeedbackBubble];
+        [self.animationManager beginLoadingAnimation];
+        
+        __weak typeof (self) weakSelf = self;
+        
+        [MerchantServer getCredentialsWithCompletion:^(PPOCredentials *credentials, NSError *retrievalError) {
+            
+            weakSelf.credentials = credentials;
+            
+            if (retrievalError) {
+                [weakSelf handleError:retrievalError];
+                return;
+            }
+            
+            /*
+             *The PaypointSDK performs paramater validation, to the best extent possible, before any network request is made.
+             */
+            [weakSelf.paymentManager makePayment:payment
+                                 withCredentials:credentials
+                                     withTimeOut:60.0f
+                                  withCompletion:[weakSelf paymentCompletionHandler]];
+            
+        }];
         
     }
         
 }
 
-#pragma mark - MerchantPaymentManager
-
-/*
- *The merchant is responsible for obtaining credentials via paypoint.
- *The 'MerchantPaymentManager' class is a dedicated class for handling credentials acquisition.
- *Credentials are acquired and retained in memory by the 'MerchantPaymentManager' class. So the merchant manager subsequently liases with the PaypointSDK, and reports back to this controller, vis it's delegate.
- */
--(MerchantPaymentManager *)paymentManager {
-    if (_paymentManager == nil) {
-        _paymentManager = [[MerchantPaymentManager alloc] initWithDelegate:self];
-    }
-    return _paymentManager;
-}
-
-#pragma mark - MerchantPaymentManager Delegate
-
--(void)paymentManager:(MerchantPaymentManager *)manager willAttemptPayment:(PPOPayment *)payment {
-    [self.animationManager hideFeedbackBubble];
-    [self.animationManager beginLoadingAnimation];
-}
-
--(void)paymentManager:(MerchantPaymentManager *)manager successfullWithOutcome:(PPOOutcome *)outcome {
-    __weak typeof(self) weakSelf = self;
-    [self.animationManager endLoadingAnimationWithCompletion:^{
-        [weakSelf performSegueWithIdentifier:@"OutcomeViewControllerSegueID" sender:outcome];
-    }];
-}
-
--(void)paymentManager:(MerchantPaymentManager *)manager didFailWithError:(NSError *)error {
-    [self handleError:error];
+-(void(^)(PPOOutcome *outcome, NSError *paymentError))paymentCompletionHandler {
+    __weak typeof (self) weakSelf = self;
+    return ^ (PPOOutcome *outcome, NSError *paymentError) {
+        if (paymentError) {
+            [weakSelf handleError:paymentError];
+        } else {
+            [weakSelf.animationManager endLoadingAnimationWithCompletion:^{
+                [weakSelf performSegueWithIdentifier:@"OutcomeViewControllerSegueID" sender:outcome];
+            }];
+        }
+    };
 }
 
 #pragma mark - Error Handling
 
 -(void)handleError:(NSError*)error {
     
-    BOOL paypointSpecific = (error && error.domain == PPOPaypointSDKErrorDomain);
+    BOOL paypointSpecificError = (error && error.domain == PPOPaypointSDKErrorDomain);
     
     __weak typeof(self) weakSelf = self;
     
     [self.animationManager endLoadingAnimationWithCompletion:^{
         
-        if (paypointSpecific) {
+        if (paypointSpecificError) {
             
             NSString *message = [error.userInfo objectForKey:NSLocalizedFailureReasonErrorKey];
             
@@ -119,27 +189,50 @@
         }
         else if ([NetworkErrorManager noNetwork:error]) {
             
-            [[[UIAlertView alloc] initWithTitle:@"Error"
-                                        message:@"Please check your internet or data connection and try again."
-                                       delegate:nil
-                              cancelButtonTitle:@"Dismiss"
-                              otherButtonTitles:nil, nil]
-             show];
+            NSString *message = @"It looks like your internet connection dropped out. Would you like to check the status of your payment?";
+            
+            [self displayPaymentQueryOption:error withMessage:message];
             
         } else {
             
-            NSString *message = [error.userInfo objectForKey:NSLocalizedFailureReasonErrorKey];
+            NSString *message = @"We were unable to determine the outcome of your payment. Would you like to check the status of your payment now ?";
             
-            [[[UIAlertView alloc] initWithTitle:@"Error"
-                                        message:(message) ?: @"Unknown error"
-                                       delegate:nil
-                              cancelButtonTitle:@"Dismiss"
-                              otherButtonTitles:nil, nil]
-             show];
+            [self displayPaymentQueryOption:error withMessage:message];
             
         }
         
     }];
+    
+}
+
+-(void)displayPaymentQueryOption:(NSError*)error withMessage:(NSString*)message {
+    
+    __weak typeof(self) weakSelf = self;
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *action;
+    
+    action = [UIAlertAction actionWithTitle:@"Check Status"
+                                      style:UIAlertActionStyleDestructive
+                                    handler:^(UIAlertAction *action) {
+                                        [weakSelf.paymentManager paymentOutcome:weakSelf.currentPayment
+                                                                withCredentials:weakSelf.credentials
+                                                                 withCompletion:[weakSelf paymentCompletionHandler]];
+                                    }];
+    
+    [alert addAction:action];
+    
+    action = [UIAlertAction actionWithTitle:@"Dismiss"
+                                      style:UIAlertActionStyleCancel
+                                    handler:nil];
+    [alert addAction:action];
+    
+    [self presentViewController:alert
+                       animated:YES
+                     completion:nil];
     
 }
 
