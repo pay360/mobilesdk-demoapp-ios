b3386dbf68b248388935a57a26e5d25b22bd7dd5
Rob Nash <robnash@paypoint.com>
2015-06-16 13:44:40 +0100

    Pay now button changes.

--- Merchant/PaymentFormViewController.h
+++ Merchant/PaymentFormViewController.h
@@ -14,5 +14,5 @@
 @property (weak, nonatomic) IBOutlet UIView *loadingView;
 @property (weak, nonatomic) IBOutlet UILabel *loadingMessageLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *paypointLogoImageView;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *payNowButton;
+@property (weak, nonatomic) IBOutlet UIButton *payNowButton;
 @end
