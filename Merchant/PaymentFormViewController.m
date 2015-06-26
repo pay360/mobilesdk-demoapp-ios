0c5c1924ecfdf06e247ff62293b529bd9edc3641
Rob Nash <robnash@paypoint.com>
2015-06-26 10:55:41 +0100

    Fixed issue where pay now can be hit repeatedly and trigger a second payment to follow the first one.

--- Merchant/PaymentFormViewController.m
+++ Merchant/PaymentFormViewController.m
@@ -41,7 +41,7 @@ typedef enum : NSUInteger {
 @property (nonatomic, weak) UITextField *fieldFirstResponder;
 @end
 
-@implementation PaymentFormViewController
+@implementation PaymentFormViewController 
 
 -(FormDetails *)form {
     if (_form == nil) {
@@ -182,8 +182,9 @@ typedef enum : NSUInteger {
                                    withCompletion:[self paymentCompletionHandler]];
                 break;
                 
-            case UI_ALERT_TRY_AGAIN:
-                [self makePayment:self.currentPayment];
+            case UI_ALERT_TRY_AGAIN: {
+                [self preparePayment:self.currentPayment];
+            }
                 break;
                 
             default:
@@ -346,46 +347,59 @@ typedef enum : NSUInteger {
         
     } else if (self.animationManager.animationState == LOADING_ANIMATION_STATE_ENDED) {
         
-        /*!
-         * The 'form' model object represents the data acquired by each FormField.h
-         * in our payment form. We utilise the PaypointSDK to perform inline validation.
-         * This is handled in our implementation of the PaymentEntryFieldsManager protocol, which can
-         * be found in our superclass.
-         */
-        FormDetails *formDetails = self.form;
+        [self preparePayment:self.currentPayment];
         
-        self.currentPayment.card = [PPOCard new];
-        self.currentPayment.card.pan = formDetails.cardNumber;
-        self.currentPayment.card.cvv = formDetails.cvv;
-        self.currentPayment.card.expiry = formDetails.expiry;
-        self.currentPayment.transaction.amount = formDetails.amount;
-        self.currentPayment.card.cardHolderName = @"Dai Jones";
+    }
+    
+}
+
+-(void)preparePayment:(PPOPayment*)payment {
+    
+    /*!
+     * The 'form' model object represents the data acquired by each FormField.h
+     * in our payment form. We utilise the PaypointSDK to perform inline validation.
+     * This is handled in our implementation of the PaymentEntryFieldsManager protocol, which can
+     * be found in our superclass.
+     */
+    FormDetails *formDetails = self.form;
+    
+    payment.card = [PPOCard new];
+    payment.card.pan = formDetails.cardNumber;
+    payment.card.cvv = formDetails.cvv;
+    payment.card.expiry = formDetails.expiry;
+    payment.transaction.amount = formDetails.amount;
+    payment.card.cardHolderName = @"Dai Jones";
+    
+    NSError *invalid = [PPOValidator validatePayment:payment];
+    
+    if (invalid) {
+        PPOOutcome *outcome = [PPOOutcome new];
+        outcome.error = invalid;
+        outcome.payment = payment;
         
-        NSError *invalid = [PPOValidator validatePayment:self.currentPayment];
+        [self handleLocalValidationOutcome:outcome];
+    } else {
+        [self.paymentFormAnimationManager beginLoadingAnimation];
         
-        if (invalid) {
-            PPOOutcome *outcome = [PPOOutcome new];
-            outcome.error = invalid;
-            outcome.payment = self.currentPayment;
+        __weak typeof(self) weakSelf = self;
+        
+        [self fetchTokenForPayment:payment withCompletion:^(NSError *error) {
             
-            [self handleLocalValidationOutcome:outcome];
-        } else {
-            [self.paymentFormAnimationManager beginLoadingAnimation];
-            
-            __weak typeof(self) weakSelf = self;
-            
-            [self fetchTokenForPayment:self.currentPayment withCompletion:^(NSError *error) {
-                
-                if (error) {
-                    [weakSelf handleErrorGeneratedByMerchantDemoApp:error];
-                } else {
-                    if (self.currentPayment.credentials) {
-                        [self makePayment:self.currentPayment];
-                    }
+            if (error) {
+                [weakSelf handleErrorGeneratedByMerchantDemoApp:error];
+            } else {
+                if (payment.credentials) {
+                    /*
+                     *The PaypointSDK performs paramater validation before any network request is made.
+                     */
+                    [weakSelf.paymentManager makePayment:payment
+                                             withTimeOut:60.0f
+                                          withCompletion:[weakSelf paymentCompletionHandler]];
+                    
                 }
-            }];
-        }
-        
+            }
+            
+        }];
     }
     
 }
@@ -401,17 +415,6 @@ typedef enum : NSUInteger {
         completion(retrievalError);
         
     }];
-    
-}
-
--(void)makePayment:(PPOPayment*)payment {
-    
-    /*
-     *The PaypointSDK performs paramater validation before any network request is made.
-     */
-    [self.paymentManager makePayment:payment
-                         withTimeOut:60.0f
-                      withCompletion:[self paymentCompletionHandler]];
     
 }
 
