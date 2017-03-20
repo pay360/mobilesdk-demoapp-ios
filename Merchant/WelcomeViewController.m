98474aeacab6faa0db12a860fc60ab74a31590f7
Rob Nash <rob@nash.codes>
2017-03-20 10:24:10 +0000

    Repaired ‘image not found in nib’ error. Removed container view as it was not needed. Re-configured constraints using manual approach by adding a UIImageView in code. The image view now appears within the centre of the device, in all orientations. The constraints scale the image within the limits of it’s maximum resolution, so it looks good on all device sizes.

--- Merchant/WelcomeViewController.m
+++ Merchant/WelcomeViewController.m
@@ -11,9 +11,6 @@
 #import "PaymentFormViewController.h"
 
 @interface WelcomeViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *pay360Label;
-@property (weak, nonatomic) IBOutlet UIImageView *pay360Logo;
-@property (weak, nonatomic) IBOutlet UIView *logoContainer;
 @property (weak, nonatomic) IBOutlet UIView *splashView;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *demoButton;
 @end
@@ -22,10 +19,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    UIImage *image = [UIImage imageNamed:@"pay360_logo_version_1"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    [imageView setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:imageView];
+    NSDictionary *views = [NSDictionary dictionaryWithObject:imageView forKey:@"view"];
+    NSArray *constraints = [self makeConstraintsWithViews:views];
+    [self.view addConstraints:constraints];
     self.demoButton.accessibilityLabel = @"DemoButton";
-    self.pay360Label.textColor = [ColourManager pay360Blue];
-    
+    [self.view bringSubviewToFront:self.splashView];
+}
+
+-(NSArray <NSLayoutConstraint *> *)makeConstraintsWithViews:(NSDictionary *)views {
+    UIImageView *imageView = views[@"view"];
+    [imageView sizeToFit];
+    NSMutableArray *collection = [@[] mutableCopy];
+    NSArray *constraints;
+    NSLayoutConstraint *constraint;
+    constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(>=20)-[view]-(>=20)-|" options:0 metrics:nil views:views];
+    [collection addObjectsFromArray:constraints];
+    constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[view]-0-|" options:0 metrics:nil views:views];
+    [collection addObjectsFromArray:constraints];
+    constraint = [NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:imageView.superview attribute:NSLayoutAttributeCenterX multiplier:1 constant:0];
+    constraint.priority = UILayoutPriorityDefaultLow;
+    [collection addObject:constraint];
+    return collection;
 }
 
 -(UIStatusBarAnimation)preferredStatusBarUpdateAnimation {
